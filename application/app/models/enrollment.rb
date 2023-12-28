3587da70092816c546892b06cde5a6c93743a9b0
BeatrizPlacido <biaplacsilva0212@gmail.com>
2023-12-27 23:20:26 -0300

    :construction: Ajustando método de criarção de faturas

--- application/app/models/enrollment.rb
+++ application/app/models/enrollment.rb
@@ -7,23 +7,34 @@ class Enrollment < ApplicationRecord
   validates :quantity_of_bills, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :bill_due_date, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
   validates :course_name, presence: true
+
   after_create :criar_faturas
 
   def criar_faturas
-    require 'time', 'date'
-
+    require 'date'
+  
     invoice_value = total_course_price / quantity_of_bills
-    current_day = Time.now
-    today = current_day.strftime("%d").to_i
-
-    if today <= bill_due_date
-      next_mounth = current_day >> 1
-      due_date = next_mounth.strftime("#{bill_due_date}/%m/%Y").to_date
+    today = Date.today
+    current_day = today.strftime("%d").to_i
+  
+    if current_day > bill_due_date
+      next_bill = today >> 1
+      due_date = next_bill.strftime("#{bill_due_date}/%m/%Y")
+      month = 1
     else
-      due_date = current_day.strftime("#{bill_due_date}/%m/%Y").to_date
-    
-    quantity_of_bills.times do
-      Fatura.create!(invoice_value: invoice_value, due_date: due_date, enrollment_id: id, status: 'Aberta')
-     
+      due_date = today.strftime("#{bill_due_date}/%m/%Y")
+      month = 0
+    end
+  
+    bills_to_create = quantity_of_bills.to_i.abs
+
+    while bills_to_create > 0 
+      next_bill = today >> month
+      due_date = next_bill.strftime("#{bill_due_date}/%m/%Y")
+      Bill.create!(invoice_value:invoice_value, due_date:due_date)
+      puts bills_to_create, due_date
+      bills_to_create -= 1
+      month += 1
+    end
   end 
 end
