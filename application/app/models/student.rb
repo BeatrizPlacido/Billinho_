4162381820274d2f2b35ae50a04267d5ccbb246d
BeatrizPlacido <biaplacsilva0212@gmail.com>
2025-02-17 22:17:14 -0300

    refactor: update payment method validation and improve error messages

--- application/app/models/student.rb
+++ application/app/models/student.rb
@@ -1,10 +1,11 @@
 class Student < ApplicationRecord
+  PERMITTED_PAYMENT_METHODS = %w[boleto card]
+
   validates :name, presence: true, uniqueness: true
-  validates :cpf, presence: true, uniqueness: true, format: {  with: /\d{11}/, message: "deve conter apenas números" }
-  validates :gender, presence: true, inclusion: { in: ["M", "F"] }
-  validates :payment_method, presence: true, inclusion: { in: ["Boleto", "Cartão"] }
+  validates :cpf, presence: true, uniqueness: true, format: {  with: /\d{11}/, message: "must contain only numbers" }
+  validates :payment_method, presence: true, inclusion: { in: PERMITTED_PAYMENT_METHODS }
   validates :number, presence: true, numericality: { only_integer: true }
-  validates :cep, presence: true, format: { with: /\d{5}-\d{3}/, message: "deve ser passado no formato 00000-000"}
+  validates :cep, presence: true, format: { with: /\d{5}-\d{3}/, message: "must be in the format 00000-000" }
 
   before_create :search_address
 
