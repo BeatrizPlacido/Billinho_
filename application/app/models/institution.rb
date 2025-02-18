d76b1e8cd80983b29113b1ded375eed98289321f
BeatrizPlacido <biaplacsilva0212@gmail.com>
2025-02-17 21:00:52 -0300

    fix: update institution model validations and improve error messages

--- application/app/models/institution.rb
+++ application/app/models/institution.rb
@@ -1,13 +1,15 @@
 class Institution < ApplicationRecord
+  PERMITTED_TYPES = %i[university  elementary_school middle_school]
+
   self.inheritance_column = :_type_disabled
 
   has_many :enrollments
 
   validates :name, presence: true, uniqueness: true
-  validates :cnpj, presence: true, uniqueness: true, format: { with: /\A\d+\z/, message: "deve conter apenas números" }
-  validates :type, inclusion: { in: ["Universidade", "Escola", "Creche"] }
+  validates :cnpj, presence: true, uniqueness: true, format: { with: /\A\d+\z/, message: "must contain only numbers" }
+  validates :type, inclusion: { in: PERMITTED_TYPES }
   validates :number, presence: true, numericality: { only_integer: true }
-  validates :cep, presence: true, format: { with: /\A\d{5}-\d{3}\z/, message: "deve ser passado no formato 00000-000"}
+  validates :cep, presence: true, format: { with: /\A\d{5}-\d{3}\z/, message: "must be in the format 00000-000" }
 
   before_create :search_address
 
